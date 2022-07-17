79b3687ec1d9689606981fc11ade808075ff96bb
Alexander <grozakm0100@protonmail.com>
2022-07-17 21:55:23 +0530

    added new branch and 2nd type of modal added

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-'use strict';
-const openModalBtn = document.querySelectorAll('[data-open-button]')
-const closeModalBtn = document.querySelector('[data-close-button]')
-const modalContainer = document.querySelector('[data-modal-container]')
+"use strict";
+const openModalBtn = document.querySelector("[data-open-button]");
+const modalContainer = document.querySelector("[data-modal-container]");
 
 const closeModal = function () {
-    modalContainer.classList.remove('show')
-}
+  modalContainer.classList.remove("show");
+};
 const openModal = function () {
-    modalContainer.classList.add('show')
-}
-for (let i = 0; i < openModalBtn.length; i++) {
-    openModalBtn[i].addEventListener('click', () => {
-        openModal()
-    })
-}
+  modalContainer.classList.add("show");
+};
 
-closeModalBtn.addEventListener('click', () => {
-    closeModal()
-})
-modalContainer.addEventListener('click', (e) => {
-    if (e.target !== modalContainer) return;
-    closeModal()
-})
+openModalBtn.addEventListener("click", () => {
+  openModal();
+});
+modalContainer.addEventListener("click", (e) => {
+  if (e.target !== modalContainer) return;
+  closeModal();
+});
 
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape') closeModal()
-})
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") closeModal();
+});
